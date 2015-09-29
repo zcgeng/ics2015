0b6e3836c4bfd60922adda411933321f5163046c
14307130116-Zhao Changgeng <zcg1996@qq.com>
2015-09-29 18:32:44 +0800

    添加功能:  单步执行(si[N])

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,12 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args) {
+    int n = 1;
+    if (args != NULL) sscanf(args,"%d",&n);
+    cpu_exec(n);
+    return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +52,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+    { "si", "si [N] top the program after N instructions.(N : default 1)" , cmd_si },
 	/* TODO: Add more commands */
 
 };
