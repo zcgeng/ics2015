3ece1971b78ddd4970893b3c92b551c941edea3f
14307130116-Zhao Changgeng <zcg1996@qq.com>
2015-11-20 14:40:50 +0800

    debug: jcc-sign extended immediate number

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,7 +1,10 @@
 #include "cpu/exec/template-start.h"
 
 #define update_eip()\
-	cpu.eip += (int32_t)op_src->val;\
+	int32_t val = op_src->val;\
+	val = val << (32 - DATA_BYTE * 8);\
+	val = val >> (32 - DATA_BYTE * 8);\
+	cpu.eip += val;\
 	if(DATA_BYTE == 2) cpu.eip &= 0xffff;
 
 #if DATA_BYTE == 1
@@ -19,7 +22,7 @@
 #define instr jo
 
 static void do_execute() {
-	if(cpu.OF == 1) update_eip();
+	if(cpu.OF == 1) {update_eip();}
 	print_asm("jo $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -30,7 +33,7 @@ make_instr_helper(i)
 #define instr jno
 
 static void do_execute() {
-	if(cpu.OF == 0) update_eip();
+	if(cpu.OF == 0) {update_eip();}
 	print_asm("jno $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -41,7 +44,7 @@ make_instr_helper(i)
 #define instr jb
 
 static void do_execute() {
-	if(cpu.CF == 1) update_eip();
+	if(cpu.CF == 1) {update_eip();}
 	print_asm("jb $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -52,7 +55,7 @@ make_instr_helper(i)
 #define instr jae
 
 static void do_execute() {
-	if(cpu.OF != 0) update_eip();
+	if(cpu.OF != 0) {update_eip();}
 	print_asm("jae $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -63,7 +66,7 @@ make_instr_helper(i)
 #define instr je
 
 static void do_execute() {
-	if(cpu.ZF == 1) update_eip();
+	if(cpu.ZF == 1) {update_eip();}
 	print_asm("je $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -74,7 +77,7 @@ make_instr_helper(i)
 #define instr jne
 
 static void do_execute() {
-	if(cpu.ZF == 0) update_eip();
+	if(cpu.ZF == 0) {update_eip();}
 	print_asm("jne $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -85,7 +88,7 @@ make_instr_helper(i)
 #define instr jbe
 
 static void do_execute() {
-	if(cpu.CF == 1 || cpu.ZF == 1) update_eip();
+	if(cpu.CF == 1 || cpu.ZF == 1) {update_eip();}
 	print_asm("jbe $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -96,7 +99,7 @@ make_instr_helper(i)
 #define instr ja
 
 static void do_execute() {
-	if(cpu.CF == 0 && cpu.ZF == 0) update_eip();
+	if(cpu.CF == 0 && cpu.ZF == 0) {update_eip();}
 	print_asm("ja $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -107,7 +110,7 @@ make_instr_helper(i)
 #define instr js
 
 static void do_execute() {
-	if(cpu.SF == 1) update_eip();
+	if(cpu.SF == 1) {update_eip();}
 	print_asm("js $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -118,7 +121,7 @@ make_instr_helper(i)
 #define instr jns
 
 static void do_execute() {
-	if(cpu.SF == 0) update_eip();
+	if(cpu.SF == 0) {update_eip();}
 	print_asm("jns $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -129,7 +132,7 @@ make_instr_helper(i)
 #define instr jp
 
 static void do_execute() {
-	if(cpu.PF == 1) update_eip();
+	if(cpu.PF == 1) {update_eip();}
 	print_asm("jp $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -140,7 +143,7 @@ make_instr_helper(i)
 #define instr jnp
 
 static void do_execute() {
-	if(cpu.PF == 0) update_eip();
+	if(cpu.PF == 0) {update_eip();}
 	print_asm("jnp $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -151,7 +154,7 @@ make_instr_helper(i)
 #define instr jl
 
 static void do_execute() {
-	if(cpu.SF != cpu.OF) update_eip();
+	if(cpu.SF != cpu.OF) {update_eip();}
 	print_asm("jl $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -162,7 +165,7 @@ make_instr_helper(i)
 #define instr jge
 
 static void do_execute() {
-	if(cpu.SF == cpu.OF) update_eip();
+	if(cpu.SF == cpu.OF) {update_eip();}
 	print_asm("jge $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -173,7 +176,7 @@ make_instr_helper(i)
 #define instr jle
 
 static void do_execute() {
-	if(cpu.ZF == 1 && cpu.SF != cpu.OF) update_eip();
+	if(cpu.ZF == 1 && cpu.SF != cpu.OF) {update_eip();}
 	print_asm("jle $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -184,7 +187,7 @@ make_instr_helper(i)
 #define instr jg
 
 static void do_execute() {
-	if(cpu.ZF == 0 || cpu.SF == cpu.OF) update_eip();
+	if(cpu.ZF == 0 || cpu.SF == cpu.OF) {update_eip();}
 	print_asm("jg $0x%x", cpu.eip + CODE_LEN);
 }
 
@@ -195,7 +198,7 @@ make_instr_helper(i)
 #define instr jcxz
 
 static void do_execute() {
-	if(cpu.ecx == 0) update_eip();
+	if(cpu.ecx == 0) {update_eip();}
 	print_asm("jcxz $0x%x", cpu.eip + CODE_LEN);
 }
 
