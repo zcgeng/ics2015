da8d70e87c9c6087ef7862c2c97379ec5b6528df
tracer-ics2015 <tracer@njuics.org>
2016-02-18 12:31:53 +0800

    >  run 14307130116 zcgeng Linux z 3.19.0-42-generic #48-Ubuntu SMP Thu Dec 17 22:54:45 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:31:53 up  2:43,  2 users,  load average: 0.21, 0.16, 0.15 e07ec095743630b84d429783da33c1653414a3ba

--- Makefile
+++ Makefile
@@ -56,7 +56,7 @@ count :
 	find . -name "*.[hc]"|xargs cat|grep -v ^$$|wc -l 
 	@echo "lines without empty lines"
 
-USERPROG := obj/testcase/add-longlong
+USERPROG := obj/testcase/add-longlong-zcg
 ENTRY := $(USERPROG)
 
 entry: $(ENTRY)
---
+++ testcase/src/add-longlong-zcg.c
@@ -0,0 +1,14 @@
+#include "trap.h"
+
+long long add(long long a, long long b) {
+	long long c = a + b;
+	return c;
+}
+int main() {
+	int a = add(0x7fffffffffffffffLL, 0);
+	nemu_assert(a == 0x7fffffffffffffffLL);
+
+	HIT_GOOD_TRAP;
+
+	return 0;
+}
