4c8506b984781ee75784bbef007ec179ccf37df9
tracer-ics2015 <tracer@njuics.org>
2016-02-27 15:20:59 +0800

    >  run 14307130116 zcgeng Linux z 3.19.0-42-generic #48-Ubuntu SMP Thu Dec 17 22:54:45 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:20:59 up  3:14,  2 users,  load average: 0.02, 0.04, 0.06 6f5b4583efc64886bb05851ad0069a99f0c9739

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_src->val & ((1ll << (op_src->size * 8)) - 1);
+	//Log("%x, %x, %x\n", (int)result, (int)op_src->val, (int)op_src->size);
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
