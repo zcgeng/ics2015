659ddf2f25bd6dbe8e62809140b427c05dc2d613
tracer-ics2015 <tracer@njuics.org>
2016-02-27 12:10:28 +0800

    >  run 14307130116 zcgeng Linux z 3.19.0-42-generic #48-Ubuntu SMP Thu Dec 17 22:54:45 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:10:28 up 4 min,  2 users,  load average: 1.01, 0.98, 0.46 bbe0a8bdffe6e0daf85f574a4e7c0de4361787fa

--- testcase/src/add-longlong-zcg.c
+++ testcase/src/add-longlong-zcg.c
@@ -1,11 +1,7 @@
 #include "trap.h"
 
-long long add(long long a, long long b) {
-	long long c = a + b;
-	return c;
-}
 int main() {
-	long long a = add(0x7fffffffffffffffLL, 0);
+	long long a = 0x7fffffffffffffffLL + 0;
 	nemu_assert(a == 0x7fffffffffffffffLL);
 
 	HIT_GOOD_TRAP;
