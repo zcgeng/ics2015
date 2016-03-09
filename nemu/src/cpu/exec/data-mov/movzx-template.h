18769f5a5e57440d3dd2331e91fdf7398f1ab6e3
14307130116-Zhao Changgeng <zcg1996@qq.com>
2016-03-09 12:43:18 +0800

    debug:movzx

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,9 +3,9 @@
 #define instr movzx
 
 static void do_execute() {
-	DATA_TYPE result = op_src->val & ((1ll << (op_src->size * 8)) - 1);
+	uint32_t result = op_src->val & ((1ll << (op_src->size * 8)) - 1);
 	//Log("%x, %x, %x\n", (int)result, (int)op_src->val, (int)op_src->size);
-	OPERAND_W(op_dest, result);
+	write_operand_l(op_dest, result);
 	print_asm_template2();
 }
 
