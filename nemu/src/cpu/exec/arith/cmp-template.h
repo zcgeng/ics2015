976c80f8f3c6b5aa410ce91c9eca5e80688da694
zcgeng <zcg1996@qq.com>
2016-03-13 23:40:24 +0800

    debug : EFLAGS of cmp

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,17 +3,14 @@
 #define instr cmp
 
 static void do_execute() {
-    uint32_t srcval = op_src->val;
-    srcval <<= 32 - DATA_BYTE * 8;
-    srcval >>= 32 - DATA_BYTE * 8;
-    DATA_TYPE result = op_dest->val - srcval;
+    DATA_TYPE result = op_dest->val - op_src->val;
     update_EFLAGS_PZS(result);
     //CF: 借位
-    cpu.CF = (op_dest->val > srcval) ? 0 : 1;
+    cpu.CF = (((long long)op_dest->val - (long long)op_src->val) >> (8 * DATA_BYTE)) & 1;
     //AF: 低三位是否借位
-    cpu.AF = (op_dest->val & 0x8) > (srcval & 0x8) ? 0 : 1;
+    cpu.AF = (op_dest->val & 0x8) > (op_src->val & 0x8) ? 0 : 1;
     //OF: overflow flag, 同号相减不会溢出;异号相减, 结果与被减数反号则溢出.
-    if( MSB(op_dest->val) != MSB(srcval) && MSB(result) != MSB(op_dest->val)) 
+    if( MSB(op_dest->val) != MSB(op_src->val) && MSB(result) != MSB(op_dest->val)) 
         cpu.OF = 1; 
     else cpu.OF = 0;
 	print_asm_template2();
