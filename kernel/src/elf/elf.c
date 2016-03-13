1c54a6beb46fa573cbb3f0a7d664cd773d2ea643
zcgeng <zcg1996@qq.com>
2016-03-13 16:20:58 +0800

    实现loader函数

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -30,25 +30,24 @@ uint32_t loader() {
 
 	elf = (void*)buf;
 
-	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	/* DONE: fix the magic number with the correct one */
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+	ph = (Elf32_Ehdr *)(buf + elf->e_phoff);
+	int i = 0;
+	for(i = 0; i < elf->e_phnum; ++i) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-			/* TODO: read the content of the segment from the ELF file 
-			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */
+			/* DONE: read the content of the segment from the ELF file to the memory region [VirtAddr, VirtAddr + FileSiz) */
+			ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
 			 
-			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
+			/* DONE: zero the memory region [VirtAddr + FileSiz, VirtAddr + MemSiz)	 */
+			memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
