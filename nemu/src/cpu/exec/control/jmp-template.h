ca8f825abe59a1f5dd37047c3d1430d81df3a521
zcgeng <zcg1996@qq.com>
2016-03-14 23:40:38 +0800

    debug : jmp

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -4,6 +4,13 @@
 
 static void do_execute() {
 	if(op_src->type == OP_TYPE_IMM){
+		if(MSB(op_src->val) == 1){
+			#if DATA_BYTE == 1
+			op_src->val |= 0xffffff00;
+			#elif DATA_BYTE == 2
+			op_src->val |= 0xffff0000;
+			#endif
+		}
 		cpu.eip += op_src->val;
 		//Log("eip = 0x%x src = 0x%x\n", cpu.eip, op_src->val);
 		if(DATA_BYTE == 2)
@@ -11,15 +18,16 @@ static void do_execute() {
 		print_asm("jmp $0x%x", cpu.eip + DATA_BYTE + 1);
 	}
 	else{
-		#if DATABYTE == 2 
-			cpu.eip = op_src->val & 0x0000ffff - 2;
+		#if DATA_BYTE == 2 
+			cpu.eip = (op_src->val & 0x0000ffff) - 2;
 		#else
 			if (op_src->type == OP_TYPE_MEM) 
-				cpu.eip = op_src->val - 1;
+				cpu.eip = op_src->val - DATA_BYTE - 3;
 			else
 				cpu.eip = op_src->val - 2;
 		#endif
-
+		//Log("eip = 0x%x src = 0x%x\n", cpu.eip, op_src->val);
+		print_asm("jmp $%s", op_src->str);
 	}
 }
 
