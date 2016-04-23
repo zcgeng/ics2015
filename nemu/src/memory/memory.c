0b34b9d59b35cef1761146fcb67a40b4874f165e
zcgeng <zcg1996@qq.com>
2016-04-23 16:24:41 +0800

    在monitor中添加L2 cache的信息

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,7 +3,6 @@
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 uint32_t cache_read(hwaddr_t, size_t);
-uint32_t cache2_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
@@ -11,13 +10,6 @@ void cache2_debug(hwaddr_t addr);
 void cache_debug(hwaddr_t addr);
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	uint32_t c1 = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
-	uint32_t c2 = cache2_read(addr, len) & (~0u >> ((4 - len) << 3));
-	if(c1 != c2){
-		Log("addr = %x, len = %d, cache1 : %x, cache2 : %x\n", addr, (int)len,c1,c2);
-		cache2_debug(addr);	
-		cache_debug(addr);		
-	}
 	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
