6fadd09b02d2fc1e30522eb03267c60d27ff33bb
tracer-ics2015 <tracer@njuics.org>
2016-04-25 22:43:18 +0800

    >  compile NEMU 14307130116 zcgeng Linux z 3.19.0-58-generic #64-Ubuntu SMP Thu Mar 17 18:30:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:43:18 up  7:04,  3 users,  load average: 0.47, 0.39, 0.66 5a69fc415d278e52d0dcf1b4c012e085e643f946

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -44,11 +44,13 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
+			uint32_t hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+
 			/* DONE: read the content of the segment from the ELF file to the memory region [VirtAddr, VirtAddr + FileSiz) */
-			ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
+			ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz); 
 			 
 			/* DONE: zero the memory region [VirtAddr + FileSiz, VirtAddr + MemSiz)	 */
-			memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
