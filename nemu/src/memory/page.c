d173ec6d68545603f05012f70acf85b978b4d8ae
zcgeng <zcg1996@qq.com>
2016-04-26 09:41:36 +0800

    debug

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -19,7 +19,6 @@ hwaddr_t page_translate(lnaddr_t addr) {
 	// first level page table - page directory entry
 	PDE dir_entry;
 	dir_entry.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
-Log("0x%x", dir_entry.page_frame);
 	Assert(dir_entry.present == 1, "dir_entry is not valid!  addr = 0x%x, dir = 0x%x", addr, lnaddr.dir);
 
 	// second level page table - page table entry
@@ -28,5 +27,5 @@ Log("0x%x", dir_entry.page_frame);
 	Assert(page_table_entry.present == 1, "page_table_entry is not valid!  addr = 0x%x, page_frame = 0x%x, page_table_entry = 0x%x", 
 			addr, dir_entry.page_frame, page_table_entry.val);
 
-	return (page_table_entry.page_frame << 12) + addr;
+	return (page_table_entry.page_frame << 12) + lnaddr.offset;
 }
