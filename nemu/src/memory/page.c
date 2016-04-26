ce369ba8b5a067058da0b0373f3679e0076fc28d
tracer-ics2015 <tracer@njuics.org>
2016-04-26 17:14:15 +0800

    >  compile NEMU 14307130116 zcgeng Linux z 3.19.0-58-generic #64-Ubuntu SMP Thu Mar 17 18:30:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:14:15 up  2:41,  2 users,  load average: 0.98, 0.55, 0.29 f67214df3300d11b762efce16e2b7227c95fac1c

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -9,7 +9,6 @@ typedef union {
 	uint32_t val;
 } line_addr;
 
-#define limit 4096
 uint32_t hwaddr_read(hwaddr_t, size_t);
 
 hwaddr_t page_translate(lnaddr_t addr) {
@@ -18,7 +17,7 @@ hwaddr_t page_translate(lnaddr_t addr) {
 
 	// first level page table - page directory entry
 	PDE dir_entry;
-	Log("0x%x", (cpu.cr3.page_directory_base << 12) + 4 * lnaddr.dir);
+	Log("addr = 0x%x, dir = 0x%x", addr, lnaddr.dir);
 	dir_entry.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + 4 * lnaddr.dir, 4);
 	Assert(dir_entry.present == 1, "dir_entry is not valid!  addr = 0x%x, dir = 0x%x", addr, lnaddr.dir);
 
