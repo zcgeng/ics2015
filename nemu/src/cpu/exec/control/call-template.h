04af5fae95012778e06b05d6ea15efada22bb70a
tracer-ics2015 <tracer@njuics.org>
2016-06-12 21:34:52 +0800

    >  compile NEMU 14307130116 zcgeng Linux z 3.19.0-58-generic #64-Ubuntu SMP Thu Mar 17 18:30:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:34:52 up  8:45,  2 users,  load average: 0.04, 0.15, 0.15 4db42d69233b8eeabf7d3b7c4ca7269dec01839f

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -8,14 +8,15 @@ static void do_execute() {
 		swaddr_write(cpu.esp, 4, cpu.eip + DATA_BYTE + 1, R_SS);// PUSH 返回地址
 		cpu.eip += op_src->val;
 		if(DATA_BYTE == 2) cpu.eip &= 0x0000ffff;
-   		 print_asm("call $0x%x", cpu.eip + DATA_BYTE + 1);
+		op_src->val += DATA_BYTE + 1;
+   		print_asm_template1();
 	}
 	else{
 		//Log("1now eip = 0x%x, esp = 0x%x\n", cpu.eip, cpu.esp);
 		swaddr_write(cpu.esp, 4, cpu.eip + 2, R_SS);
 		cpu.eip = (op_src->val) - 2;
 		//Log("2now eip = 0x%x\n", cpu.eip);
-		print_asm("call $0x%x", cpu.eip + 2);
+		print_asm_template1();
 	}	
 
 	//print_asm_template1();
