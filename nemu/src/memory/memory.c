a74cb981a18ba0fac0171846bc5a61b48e5a8c49
zcgeng <zcg1996@qq.com>
2016-06-13 16:59:05 +0800

    在内存读写函数中添加IO访问函数

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -2,22 +2,32 @@
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
+
 uint32_t cache_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
+
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
+
 lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg);
 hwaddr_t page_translate(lnaddr_t addr);
+uint32_t is_mmio(hwaddr_t);
 
 /* Memory accessing interfaces */
 void cache2_debug(hwaddr_t addr);
 void cache_debug(hwaddr_t addr);
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int num = is_mmio(addr);
+	if (num == -1) return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	else return mmio_read(addr, len, num) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	//dram_write(addr, len, data);
-	cache_write(addr, len, data);
+	int num = is_mmio(addr);
+	if(num == -1) cache_write(addr, len, data);
+	else mmio_write(addr, len, data, num);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
