bec2719c70c02ac079ab9c394c9d62f0e3aeb5ac
tracer-ics2015 <tracer@njuics.org>
2016-06-14 02:32:26 +0800

    >  compile NEMU 14307130116 zcgeng Linux z 3.19.0-58-generic #64-Ubuntu SMP Thu Mar 17 18:30:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  02:32:25 up 12:40,  3 users,  load average: 0.57, 0.34, 0.52 4b2bdca9b87425716802d7a7e7cdf4cd942fd9b6

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -34,7 +34,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
 	if(cpu.cr0.paging == 0) return hwaddr_read(addr, len);
 	if ((addr & 0xfff) + len > 0x1000) {
-		//Log("hehedadada");
+		Log("hehedadada");
 		uint32_t off = addr & 0xfff;
 		hwaddr_t hwaddr2;
 		hwaddr_t hwaddr = page_translate(addr);
@@ -51,7 +51,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
 	if(cpu.cr0.paging == 0) return hwaddr_write(addr, len, data);
 	if ((addr & 0xfff) + len > 0x1000) {
-		//Log("heheda");
+		Log("heheda");
 		uint32_t off = addr & 0xfff;
 		hwaddr_t hwaddr2;
 		hwaddr_t hwaddr = page_translate(addr);
