24dfd61b7a4b9f6469d2e61abddf34e8f7c4ab88
tracer-ics2015 <tracer@njuics.org>
2016-06-15 00:33:14 +0800

    >  compile NEMU 14307130116 zcgeng Linux z 3.19.0-58-generic #64-Ubuntu SMP Thu Mar 17 18:30:04 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:33:14 up  5:40,  2 users,  load average: 0.62, 0.41, 0.36 9145b0dcdebcc7105db36feb6857c28440ea0dc8

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -42,7 +42,28 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_write: sys_write(tf); break;
 
 		/* TODO: Add more system calls. */
-
+		case SYS_open:
+					  if(tf->ebx >= 3)tf->eax = fs_open((void *)tf->ebx , tf->ecx);
+					  //tf->eax = fs_open((void *)tf->ebx , tf->ecx);
+					  break;
+						
+		case SYS_read:
+					  if(tf->ebx >= 3)tf->eax = fs_read(tf->ebx ,(void *)tf->ecx , tf->edx);
+					  //tf->eax = fs_read(tf->ebx ,(void *)tf->ecx , tf->edx);
+					  break;
+		case SYS_lseek:
+					  if(tf->ebx >= 3)tf->eax = fs_lseek(tf->ebx , tf->ecx , tf->edx);
+					  //tf->eax = fs_lseek(tf->ebx , tf->ecx , tf->edx);
+						break;
+		case SYS_close:
+					  if(tf->ebx >= 3)tf->eax = fs_close(tf->ebx);
+					  //tf->eax = fs_close(tf->ebx);
+					  break;
+		case SYS_write: if(tf->ebx < 3)
+						  sys_write(tf);
+						else 
+						  tf->eax = fs_write(tf->ebx , (void *)tf->ecx , tf->edx);
+						break; 
 		default: panic("Unhandled system call: id = %d", tf->eax);
 	}
 }
